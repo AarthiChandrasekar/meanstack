6bafa9953ef6012792ec1883b2ee2273481bdbc1
Chandrasekar <aarthi.chandrasekar@iqvia.com>
2021-08-05 18:21:03 +0530

    first commit

---
+++ Day-3 Javascript/asg.js
@@ -0,0 +1,6 @@
+console.log('My script');
+document.getElementById("hid").innerHTML="Input";
+document.getElementById("hid").style.color='blue';
+function changeMe(){
+    document.getElementById("hid").style.backgroundColor='tomato';
+}
